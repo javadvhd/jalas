3627d62fe5e9874bdd392fd26d1861e76100b008
hosein <hosein.norouzi76@gmail.com>
2019-12-01 22:31:53 +0330

    chore(config): Some configuration file changed

---
+++ backend/services/analitics/src/app.js
@@ -0,0 +1,39 @@
+// app
+const koa = require('koa')
+const app = new koa()
+
+// cors
+const cors = require('@koa/cors')
+app.use(cors({}))
+
+// bodyParser
+const bodyParser = require('koa-bodyparser')
+app.use(bodyParser())
+
+// db
+const database = require('./database/dbConnector')
+database.connect('meeting_db')
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (error) {
+    ctx.status = error.status || 500
+    console.log('error : ', error.message)
+  }
+})
+
+// router
+const Router = require('@koa/router')
+const routerHandler = require('./router')
+
+const router = new Router()
+routerHandler(router)
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+const port = 4221
+const server = app.listen(port)
+
+module.exports = server
