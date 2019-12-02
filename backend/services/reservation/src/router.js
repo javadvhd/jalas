c15974b9ec3a97922d9656d7a164fd18f29099ca
hosein <hosein.norouzi76@gmail.com>
2019-12-02 23:03:26 +0330

    fix(reservation): Bug fixed

--- backend/services/reservation/src/router.js
+++ backend/services/reservation/src/router.js
@@ -1,20 +1,18 @@
 // helper
-const { reservationGet, post } = require('./helper')
+const { get, post } = require('axios')
 
 module.exports = router => {
   router.post('/RESERVATION_RESERVE_ROOM', async ctx => {
     const { username, room, start, end } = ctx.request.body.payload
-    console.log('11 ', username, room, start, end)
-    console.log(`http://213.233.176.40/rooms/${room}/reserve`)
     try {
-      // const url = `/room/${room}/reserve`
-      const data = await post(`http://213.233.176.40/rooms/${room}/reserve`, {
-        username: 'rkhosravi',
-        start: '2019-09-13T19:00:00',
-        end: '2019-09-13T20:00:00',
-      }).then(res => res.data)
-
-      console.log('22 ', data)
+      const { data } = await post(
+        `http://213.233.176.40/room/${room}/reserve`,
+        {
+          username,
+          start,
+          end,
+        },
+      )
       ctx.body = data
     } catch (e) {
       ctx.body = { error: e }
@@ -23,14 +21,11 @@ module.exports = router => {
 
   router.get('/RESERVATION_AVAILABLE_ROOMS', async ctx => {
     const { start, end } = JSON.parse(ctx.query.payload)
-
     try {
-      await reservationGet(
+      const { data } = await get(
         'http://213.233.176.40/available_rooms' + `?start=${start}&end=${end}`,
       )
-        .then(res => res.data)
-        .then(data => (ctx.body = data.availableRooms))
-        .catch(console.log)
+      ctx.body = data.availableRooms
     } catch (e) {
       ctx.body = { error: e }
     }
