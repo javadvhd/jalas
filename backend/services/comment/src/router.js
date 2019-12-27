25c7cb0816c1fec6b53c55a0dd8d5984c74d9784
hosein <hosein.norouzi76@gmail.com>
2019-12-27 14:11:45 +0330

    feat(comment): Backend

---
+++ backend/services/comment/src/router.js
@@ -0,0 +1,30 @@
+// db functions
+const {
+  createComment,
+  getCommentsByMeetingId,
+  deleteComment,
+} = require('./database/dbFunctions')
+
+module.exports = router => {
+  router.post('/COMMENT_CREATE', async ctx => {
+    const { meetingId, body, userId } = ctx.request.body.payload
+
+    ctx.body = await createComment({ meetingId, body, userId })
+  })
+
+  router.get('/COMMENT_GET_BY_MEETING_ID', async ctx => {
+    const { meetingId } = JSON.parse(ctx.query.payload)
+    ctx.body = await getCommentsByMeetingId(meetingId)
+  })
+
+  router.post('/COMMENT_DELETE', async ctx => {
+    const { meetingId, commentId, writerId, isAdmin } = ctx.request.body
+    const { nModified } = await deleteComment({
+      meetingId,
+      commentId,
+      writerId,
+      isAdmin,
+    })
+    ctx.status = nModified ? 200 : 401
+  })
+}
--- backend/services/user/src/router.js
+++ backend/services/user/src/router.js
@@ -4,14 +4,14 @@ const cryptography = require('cryptography')
 
 module.exports = router => {
   router.get('/USER_GET_USER_BY_ID', async ctx => {
-    const { userId } = ctx.query.payload
+    const { userId } = JSON.parse(ctx.query.payload)
     const user = await findUserById(userId)
     ctx.body = user
     ctx.status = 200
   })
 
   router.get('/USER_GET_EMAIL_BY_ID', async ctx => {
-    const { userId } = ctx.query.payload
+    const { userId } = JSON.parse(ctx.query.payload)
     const { email } = await findUserById(userId)
     ctx.body = email
     ctx.status = 200
