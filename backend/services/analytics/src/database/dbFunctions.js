7be925f34d83fc8fa9048926cf9ca9d9092df8c3
hosein <hosein.norouzi76@gmail.com>
2020-01-10 12:46:10 +0330

    feat(analytics): API for admin panel

--- backend/services/analytics/src/database/dbFunctions.js
+++ backend/services/analytics/src/database/dbFunctions.js
@@ -1,3 +1,22 @@
 const { Data } = require('./dbModel')
 
 exports.createData = payload => new Data(payload).save()
+
+exports.getCreationTime = () =>
+  Data.find({ 'data.type': 'creationTime' })
+    .select('data.length')
+    .lean()
+
+exports.getNumberOfReservedRooms = () =>
+  Data.count({ 'data.type': 'reserveCounter' })
+
+exports.getThroughput = () =>
+  Data.count({
+    'data.type': 'responseTime',
+    createdAt: { $gt: new Date() - 1000 * 60 * 60 },
+  })
+
+exports.getAverageResponseTime = () =>
+  Data.find({ 'data.type': 'responseTime' })
+    .select('data.duration')
+    .lean()
